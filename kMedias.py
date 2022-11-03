3ec509b3d17e2da7d189a7b5dcd2eb79171ee9a2
JoaquinPettinari <joaquinpettinari@hotmail.com>
2022-11-03 20:20:04 -0300

    Refactor code

--- kMedias.py
+++ kMedias.py
@@ -8,101 +8,46 @@ def poner_clase_aleatoria(conjunto, k):
         lista_clases.append(random.randrange(k))
     return conjunto.assign(sigdz=lista_clases)
 
-def kMediasNp(conjunto_original, k, conjunto_actualizado, iter=0):
-    conjunto_original_clase_primaria = quedarse_valores_clase_primaria(conjunto_original)
-    conjunto_actualizado_clase_primaria = quedarse_valores_clase_primaria(conjunto_actualizado)
-    centroides = []
-    print(conjunto_original_clase_primaria)
-    print(conjunto_actualizado_clase_primaria)
+def kMediasNp(conjunto_original, k, conjunto_actualizado=np.zeros(0)):
+    # Lista de centroides
     # Cantidad de columnas del conjunto, en el ejemplo son 4. Se le resta 1 para sacarle la clase primaria
     cantidad_columnas = conjunto_original.shape[1] - 1
-    if(np.all(conjunto_original == conjunto_actualizado) and iter != 0):
-        return conjunto_actualizado
-    else:
+
+    while not np.all(conjunto_original == conjunto_actualizado):
+        centroides = []
+        if not conjunto_actualizado:
+            conjunto_actualizado = conjunto_original 
+        else:
+            conjunto_original = conjunto_actualizado
         for i in range(k):
+            resultados_centroide_xi = []
             # Por cada iteración se queda con la clase que le digamos.
-            centroide_clase_i = filtrar_por_clase_primaria_v2(conjunto_actualizado, i)
+            conjuntos_clase_i = filtrar_por_clase_primaria_v2(conjunto_original, i)
             # Para hacer el algoritmo, sacamos la clase primaria
-            centroide_sin_clase_primaria = sacar_clase_primaria_v2(centroide_clase_i)
-            suma_total_columna_xi = []
+            conjuntos_i_sin_clase_primaria = sacar_clase_primaria_v2(conjuntos_clase_i)
 
             # Se pone el -1 para no iterar la clase primaria
             for columna in range(cantidad_columnas):
                 #Sumamos todos los xi de cada columna 
-                suma_total_columna_xi.append(centroide_sin_clase_primaria[:,columna].sum())
-            # Se calcula la posición del centroide
-            centroides.append(list(map(lambda x: x / len(centroide_sin_clase_primaria), suma_total_columna_xi)))
-        
+                suma_total_columna = conjuntos_i_sin_clase_primaria[:,columna].sum()
+                # Se calcula la posición del centroide            
+                resultados_centroide_xi.append(suma_total_columna / len(conjuntos_i_sin_clase_primaria))
+            centroides.append(resultados_centroide_xi)
+        print(centroides)
         nuevas_clases = []
         # Por fila
-        for i in range(len(conjunto_actualizado)):
+        for i in range(len(conjunto_original)):
             # Distancias por fila
             distancias = np.zeros(len(centroides))
             # Valores del centroide
             for c, centroide in enumerate(centroides):
                 # Valor de la columna
                 for j in range(cantidad_columnas):
-                    distancias[c] += (centroide[j] - conjunto_actualizado[i,j]) ** 2
+                    distancias[c] += (centroide[j] - conjunto_original[i,j]) ** 2
                 
             distancias = list(map(lambda distancia: distancia ** 1/2, distancias))
             #conjunto_actualizado[i,cantidad_columnas]= distancias.index(min(distancias))
             conjunto_actualizado[i,4] = distancias.index(min(distancias))
             nuevas_clases.append(distancias.index(min(distancias)))
-        """
-        print(quedarse_valores_clase_primaria(conjunto_original))
-        print("--------------------")    
-        print(quedarse_valores_clase_primaria(conjunto_actualizado))
-        print(quedarse_valores_clase_primaria(conjunto_original) == quedarse_valores_clase_primaria(conjunto_actualizado) )
-        """
-        kMediasNp(conjunto_original, k, conjunto_actualizado, iter+1)
-
-"""
-import random
-import numpy as np
-from utils import filtrar_por_clase_primaria, sacar_clase_primaria_np
-
-def poner_clase_aleatoria(conjunto, k):
-    lista_clases = []
-    for i in range(len(conjunto)):
-        lista_clases.append(random.randrange(k))
-    return conjunto.assign(sigdz=lista_clases)
-
-def kMedias(conjunto_original, columnas, k, conjunto_actualizado=np.array(0)):
-    centroides = []
-    #Convertimos a np para trabajar mas fácil
-    if(np.all(conjunto_original == conjunto_actualizado)):
-        print(conjunto_actualizado)
-        return conjunto_actualizado;
-    else:
-        if(conjunto_actualizado == [0]):
-            conjunto_actualizado = conjunto_original
-        #Itera por cada k
-        for i in range(k):
-            # Por cada iteración se queda con la clase que le digamos.
-            centroide_clase_i = filtrar_por_clase_primaria(conjunto_actualizado, i)
-            # Para hacer el algoritmo, sacamos la clase primaria
-            centroide_sin_clase_primaria = sacar_clase_primaria_np(centroide_clase_i)
-            # 
-            suma_total_columna_xi = []
-
-            for columna in range(len(columnas)):
-                suma_total_columna_xi.append(centroide_sin_clase_primaria[:,columna].sum())
-
-            centroides.append(list(map(lambda x: x / len(centroide_sin_clase_primaria), suma_total_columna_xi)))
-        
-        #print(centroides)
-        # Por fila
-        for i in range(len(conjunto_actualizado)):
-            # Distancias por fila
-            distancias = np.zeros(len(centroides))
-            # Valores del centroide
-            for c, centroide in enumerate(centroides):
-                # Valor de la columna
-                for j in range(len(columnas)):
-                    distancias[c] += (centroide[j] - conjunto_actualizado[i,j]) ** 2
-                
-            distancias = list(map(lambda distancia: distancia ** 1/2, distancias))
-            conjunto_actualizado[i,len(columnas)]= distancias.index(min(distancias))            
-        return kMedias(conjunto_original, k, conjunto_actualizado)
-
-"""
+    #print(nuevas_clases)
+    return conjunto_actualizado
--- main.py
+++ main.py
@@ -13,31 +13,9 @@ actual_X.dropna(subset=['choleste'], inplace=True)
 k = 2
 actual_con_clase_aleatoria = poner_clase_aleatoria(actual_X, k)
 actual_estandarizado = estandarizar_atributos(actual_con_clase_aleatoria, ['age', 'cad.dur', 'choleste' ]).to_numpy()
-a = kMediasNp(actual_estandarizado, k, actual_estandarizado)
-print(a)
-#kMedias(actual_con_clase_aleatoria, k)
-
-"""
-import numpy as np
-import pandas as pd
-from kMedias import poner_clase_aleatoria, kMedias
-from utils import sacar_clase_primaria_pd
-
-conjunto = pd.read_csv("acath.csv", encoding= 'unicode_escape')
-actual_Y = conjunto.iloc[:, -1]
-actual_X = sacar_clase_primaria_pd(conjunto)
-columnas = actual_X.keys()
-# Reemplaza los vacíos por NaN
-actual_X["choleste"].replace('', np.nan, inplace=True)
-# Borra los NaN
-actual_X.dropna(subset=['choleste'], inplace=True)
-
-k = 2
-
-actual_con_clase_aleatoria = poner_clase_aleatoria(actual_X, k).to_numpy()
-kMedias(actual_con_clase_aleatoria, columnas, k)
-
-
-"""
+a = kMediasNp(actual_estandarizado, k)
+#print(actual_estandarizado)
+#print("-----------")
+#print(a)
 
 
